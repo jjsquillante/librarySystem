1bc7cd646b07879ac1748e60ea341fab682b1975
James Squillante <jjsquillante@gmail.com>
2017-01-04 17:51:53 -0600

    commit librarySystem.js file

---
+++ librarySystem.js
@@ -0,0 +1,35 @@
+  function librarySystem(libraryName, dependency, callback) {
+  	if (arguments.length > 1) {
+  		// register
+  		if (dependency.length) {
+  			libraryStorage[libraryName] = {
+  				callback: callback,
+  				dependency: dependency
+  			};
+  		} else {
+  			libraryStorage[libraryName] = callback();
+  		}
+  	} else {
+  		// return
+  		return loadLibraries(libraryName);
+  	}
+  }
+
+  function loadLibraries(libraryName) {
+  	var dependencies = [];
+  	if (!!libraryStorage[libraryName].dependency) {
+  		var libraryDependency = libraryStorage[libraryName].dependency
+  		for (var i = 0; i < libraryDependency.length; i++) {
+  			if (libraryStorage.hasOwnProperty(libraryDependency[i])) {
+  				dependencies.push(libraryStorage[libraryDependency[i]]);
+  			};
+  		}
+  		return libraryStorage[libraryName] = libraryStorage[libraryName].callback.apply(null, dependencies);
+  	}
+  	return libraryStorage[libraryName];
+  }
+
+  function libReset() {
+  	libraryStorage = {};
+  	libraryToProcessLater = [];
+  }
